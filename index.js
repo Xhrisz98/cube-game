c800a1f1480bc46845cd6b24deceb35c4fd9f962
Christian Paul <107133521+Xhrisz98@users.noreply.github.com>
2023-09-11 13:38:24 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const newGame  = document.querySelector('.new-game');
+const rollDice = document.querySelector('.roll-dice');
+const hold = document.querySelector('.hold');
+
+let scoreFirstPlayer = document.querySelector('.score-first-player');
+let scoreSecondPlayer = document.querySelector('.score-second-player');
+
+let currentScoreFirstPlayer = document.querySelector('.current-first-score');
+let currentScoreSecondPlayer = document.querySelector('.current-second-score');
+
